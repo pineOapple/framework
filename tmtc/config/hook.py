459ee67f19ba9206e2cb7331162134eb6f000b38
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-08-17 17:23:17 +0200

    bump deps

--- tmtc/config/hook.py
+++ tmtc/config/hook.py
@@ -4,11 +4,11 @@ from common_tmtc.config.definitions import TM_SP_IDS
 from common_tmtc.config.hook_implementation import CommonFsfwHookBase
 from common_tmtc.pus_tc.cmd_definitions import common_fsfw_service_op_code_dict
 from tmtccmd.com_if import ComInterface
-from tmtccmd.config import TmTcDefWrapper
+from tmtccmd.config import TmtcDefinitionWrapper
 
 
 class FsfwHookBase(CommonFsfwHookBase):
-    def get_tmtc_definitions(self) -> TmTcDefWrapper:
+    def get_tmtc_definitions(self) -> TmtcDefinitionWrapper:
         return common_fsfw_service_op_code_dict()
 
     def assign_communication_interface(self, com_if_key: str) -> Optional[ComInterface]:
